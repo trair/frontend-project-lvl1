953ea031386ee97b93ab58d33a2e368efed5e6f8
Maxim Nikonov <the_roffler@mail.ru>
2022-06-21 20:38:57 +0300

    fix even.js

--- src/even.js
+++ src/even.js
@@ -9,10 +9,10 @@ const startGame = () => {
   console.log(`Answer "yes" if the number is even, otherwise answer "no".`);
 
   const even = (num) => num % 2 === 0;
-  const question = getRandomNum();
-  const answer = even(question) ? 'yes' : 'no';
 
   for (let i = 0; i < 3; i += 1) {
+    const question = getRandomNum();
+    const answer = even(question) ? 'yes' : 'no';
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question(`Your answer: `);
     if (userAnswer === answer) {
