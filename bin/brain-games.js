b58fb729b47a852c479ac3efd3df7cb0eefc114d
Maxim Nikonov <the_roffler@mail.ru>
2022-06-26 15:00:26 +0300

    fix

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import { askNameAndGreet } from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
+import { askNameAndGreet } from '../src/cli.js';
 
 askNameAndGreet();
