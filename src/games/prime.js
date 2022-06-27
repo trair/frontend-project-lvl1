84bd284bc277e404d429cb553b5214b2e3f47911
Maxim Nikonov <the_roffler@mail.ru>
2022-06-27 14:25:01 +0300

    fix

--- src/games/prime.js
+++ src/games/prime.js
@@ -32,7 +32,7 @@ const startGame = () => {
             console.log(`Correct!`);
         } else {
             console.log(`${userAnswer} is wrong answer  ;(. Correct answer was ${answer}`);
-            console.log(`Let's try again, ${userName}`);
+            console.log(`Let's try again, ${userName}!`);
             return;
         }
 
--- src/games/progression.js
+++ src/games/progression.js
@@ -36,7 +36,7 @@ const startGame = () => {
             console.log(`Correct!`);
         } else {
             console.log(`${userAnswer} is wrong answer  ;(. Correct answer was ${answer}`);
-            console.log(`Let's try again, ${userName}`);
+            console.log(`Let's try again, ${userName}!`);
             return;
         }
     }
