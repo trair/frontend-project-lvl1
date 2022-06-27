07755e876efd36dd3d2c56496c0025eca58de35e
Maxim Nikonov <the_roffler@mail.ru>
2022-06-27 14:23:05 +0300

    fix

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -6,14 +6,21 @@ import askNameAndGreet from '../cli.js';
 
 const userName = askNameAndGreet();
 
+const isGCD = (num1, num2) => {
+    if (num2 === 0) {
+      return num1;
+    }
+    return isGCD(num2, num1 % num2);
+  };
+
 const startGame = () => {
     console.log(`Find the greatest common divisor of given numbers.`);
 
     for (let i = 0; i < 3; i += 1) {
-        const num = getRandomNum();
         const num1 = getRandomNum();
-        const question = `${num} ${num1}`;
-        const answer = num % num1;
+        const num2 = getRandomNum();
+        const question = `${num1} ${num2}`;
+        const answer = isGCD(num1, num2);
         console.log(`Question: ${question}`);
         const userAnswer = readlineSync.question(`Your answer: `);
         if (userAnswer === String(answer)) {
